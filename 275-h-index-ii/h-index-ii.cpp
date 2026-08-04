0faf1a545f0451604cf5aac27b83da44db9aedea
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-08-04 21:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (12.03%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations) {
-        int n=citations.size();
+    int hIndex(vector<int>& arr) {
+        int n=arr.size();
         int low=0;
-        int ans=-1;
-        int high=citations[n-1];
+        int high=n-1;
         while(low<=high){
-            int h=(low+high)/2;
-            if(function(citations,h,n)){
-                low=h+1;
-                ans=h;
+            int mid=(low+high)/2;
+            if(arr[mid]>=n-mid){
+                high=mid-1;
             }
             else{
-                high=h-1;
+                low=mid+1;
             }
         }
-        return ans;
-    }
-    bool function(vector<int>& citations,int h,int n){
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(citations[i]<h) cnt++;
-            else break;
-        }
-        if((n-cnt)>=h) return true;
-        else return false;
+        return n-low;
     }
 };
