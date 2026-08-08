516db3fa12abd9352596e7714d6e877e6f65fb86
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-08-08 20:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (34.62%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         stack<char> st;
+        string ans;
         for(auto ch:s){
             if(st.empty() || st.top()!=ch){
                 st.push(ch);
+                ans.push_back(ch);
             }
             else{
                 st.pop();
+                ans.pop_back();
             }
         }
-        string ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
