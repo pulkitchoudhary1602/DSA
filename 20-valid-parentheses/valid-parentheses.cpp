8f4fe1de72b4ee4f80288d0241f4496358326092
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-08-08 20:54:47 +0530

    Time: 4 ms (1.47%) | Memory: 8.9 MB (38.16%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+       for(auto ch:s){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+                if((st.top()=='(' && ch==')') || (st.top()=='{' && ch=='}') || (st.top()=='[' && ch==']') ){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+       }
+       return st.empty();
+    }
+};
