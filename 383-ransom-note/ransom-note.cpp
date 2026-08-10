8022060af57365dc92f85d1a73432dd808093bfd
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-08-10 23:56:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (75.23%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int>mp;
-        unordered_map<char,int>mp1;
-        for(auto it:ransomNote)mp[it]++;
-        
-        for(auto it:magazine)mp1[it]++;
-        
-        for(auto it:mp){
-            if(it.second>mp1[it.first])return false;
+        vector<int>freq(26 ,0);
+        for(char c : magazine)
+        {
+            freq[c-'a']++ ;
         }
-        return true;
+        for(char c : ransomNote)
+        {
+            if(freq[c-'a'] == 0)
+           return  false ;
+
+ freq[c-'a']-- ;
+        }
+        return true ;
     }
 };
