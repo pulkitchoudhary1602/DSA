1e4b288b074a85dd4ba67c101157efb8caca1201
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-08-25 21:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 24.8 MB (55.25%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
@@ -15,5 +16,25 @@ public:
             }
             multiple=multiple+k;
         }
+    }
+};
+*/
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> check(n+2,0);
+        for(int i=0;i<n;i++){
+            if(nums[i]%k==0 && nums[i]/k<=n+1){
+                check[nums[i]/k]=1;
+            }
+        }
+        for(int i=1;i<=n+1;i++){
+            if(check[i]==0){
+                return i*k;
+            }
+        }
+        return -1;
     }
 };
