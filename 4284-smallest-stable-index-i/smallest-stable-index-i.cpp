7632295f6eac08f43cab26fe63514b4e3c896a33
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-09-05 00:07:15 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (56.73%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -1,17 +1,38 @@
+// class Solution {
+// public:
+//     int firstStableIndex(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             int maxi=0;
+//             int mini=INT_MAX;
+//             for(int j=0;j<=i;j++){
+//                 maxi=max(maxi,nums[j]);
+//             }
+//             for(int j=i;j<n;j++){
+//                 mini=min(mini,nums[j]);
+//             }
+//             if((maxi-mini)<=k){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
+        vector<int>arr(n);
+        arr[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            arr[i]=min(nums[i],arr[i+1]);
+        }
+        int maxi=0;
         for(int i=0;i<n;i++){
-            int maxi=0;
-            int mini=INT_MAX;
-            for(int j=0;j<=i;j++){
-                maxi=max(maxi,nums[j]);
-            }
-            for(int j=i;j<n;j++){
-                mini=min(mini,nums[j]);
-            }
-            if((maxi-mini)<=k){
+            maxi=max(maxi,nums[i]);
+            if(maxi-arr[i]<=k){
                 return i;
             }
         }
