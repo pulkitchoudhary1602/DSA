a42d4729e2a683d72db038773eff3679edbdded8
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-09-05 21:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (55.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int trap(vector<int>& arr) {
@@ -16,5 +17,37 @@ public:
             }
         }
         return ans;
+    }
+};
+*/
+
+class Solution {
+public:
+    int trap(vector<int>& arr) {
+       int n=arr.size();
+       int l=0,r=n-1;
+       int ans=0;
+       int lmax=0,rmax=0;
+       while(l<r){
+        if(arr[l]<=arr[r]){
+            if(lmax>arr[l]){
+                ans+=lmax-arr[l];
+            }
+            else{
+                lmax=arr[l];
+            }
+            l++;
+        }
+        else{
+            if(rmax>arr[r]){
+                ans+=rmax-arr[r];
+            }
+            else{
+                rmax=arr[r];
+            }
+            r--;
+        }
+       }
+       return ans;
     }
 };
