6f0f5b1dad891ed3324fcff8cc6bc011e9a413c3
Pulkit Choudhary <pulkitchoudhary1602@gmail.com>
2026-09-06 22:36:50 +0530

    Time: 36 ms (14.55%) | Memory: 87.6 MB (14.29%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& arr) {
+       int n=arr.size();
+       vector<int>nse(n);
+       vector<int>pse(n);
+       fnse(arr,nse,n);
+       fpse(arr,pse,n);
+       int maxi=0;
+       for(int i=0;i<n;i++){
+        maxi=max(maxi,arr[i]*(nse[i]-pse[i]-1));
+       }
+       return maxi; 
+    }
+    void fnse(vector<int>& arr,vector<int>& nse,int n){
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                nse[i]=n;
+            }
+            else{
+                nse[i]=st.top();
+            }
+            st.push(i);
+        }
+    }
+    void fpse(vector<int>& arr,vector<int>& pse,int n){
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                pse[i]=-1;
+            }
+            else{
+                pse[i]=st.top();
+            }
+            st.push(i);
+        }
+    }
+};
